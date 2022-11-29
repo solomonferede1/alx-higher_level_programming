1bbf698c373e796e406e6920a087b6b76d4830b7
solomon1227 <solomonf1227@gmail.com>
2022-11-29 17:12:35 +0300

    print combination of 3 int numbers

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{}{}".format(i, j), end="")
+        if not (i == 8 and j == 9):
+            print(",", end=" ")
+print()
