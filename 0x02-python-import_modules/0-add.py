7fe5ef8b1ac9607aef5e8cea14d166cd282675ac
solomon1227 <solomonf1227@gmail.com>
2022-12-03 08:44:24 +0300

    function which adds to integers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
 if __name__ == "__main__":
-    a, b = 1, 2
+    a = 1
+    b = 2
     sum = add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, sum))
 
