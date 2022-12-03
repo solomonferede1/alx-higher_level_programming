f573ed09e0e072fcfd2d3508f2a377e36cb3449a
solomon1227 <solomonf1227@gmail.com>
2022-12-03 10:06:56 +0300

    dir - hidden discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4 as hidden
+    hide_list = dir(hidden)
+    for lst in hide_list:
+        if lst[0:2] == "__":
+    	    print(lst)
