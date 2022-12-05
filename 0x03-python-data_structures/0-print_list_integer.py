60a7f9fe9401d6ea2271cd1a361906e64fcac225
solomon1227 <solomonf1227@gmail.com>
2022-12-05 09:47:17 +0300

    print element

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for lst in my_list:
-        print("{}".format(lst))
+if __name__ == "__main__":
+    def print_list_integer(my_list=[]):
+        for lst in my_list:
+            print("{}".format(lst))
