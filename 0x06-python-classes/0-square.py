0a8147b80146bdbccaa8426ddc8adfebbcd0cf5e
solomon1227 <solomonf1227@gmail.com>
2022-12-20 21:52:14 +0300

    Pycodestyle corrected

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 '''This module defines an empty square class'''
-#!/usr/bin/python3
+
+
 class Square:
     '''An empty class Square that defines a square'''
     pass
