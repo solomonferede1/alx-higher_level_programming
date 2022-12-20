7b14f92235472e7d873240c38957f74c8c243d37
solomon1227 <solomonf1227@gmail.com>
2022-12-20 22:05:11 +0300

    File creating private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''This module defines an empty square class'''
+
+
+class Square:
+    '''An empty class Square that defines a square'''
+    def __init__(self, size):
+        '''Initialize the size of a square
+
+        Args:
+            size(int): Size of the square
+
+        '''
+        self.__size = size
