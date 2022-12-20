32d51d709cd3e3e07dd482d3b7552b4cc67dd4f7
solomon1227 <solomonf1227@gmail.com>
2022-12-20 22:35:01 +0300

    Method that determine the area are added in the square class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''this module defines a square by: (based on 1-square.py)
+
+    Private instance attribute: size
+    Instantiation with optional size: def __init__(self, size=0):
+    size must be an integer, otherwise raise a TypeError exception with
+    the message size must be an integer
+    if size is less than 0, raise a ValueError exception
+    with the message size must be >= 0
+
+    It also determine the area of the square
+    '''
+
+
+class Square:
+    '''A Square class that defines a square and initialize
+    the size attribute'''
+    def __init__(self, size=0):
+        '''Initialize the size of a square
+
+        Args:
+            size(int): Size of the square
+
+        '''
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        '''Determine the area of a square and return it'''
+        return self.__size ** 2
