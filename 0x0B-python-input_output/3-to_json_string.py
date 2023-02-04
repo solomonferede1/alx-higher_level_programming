f11cd13050dfb53c1cb366cee33d05efde0ed3ee
solomon1227 <solomonf1227@gmail.com>
2023-02-04 15:59:55 +0300

    change to json representation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""3-to_json_string module
+change the string to json representaion
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ Change to json representation"""
+    return (json.dumps(my_obj))
