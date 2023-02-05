9dc314620705bf4f730b2733af535d21c39f5fdf
solomon <solomonf1227@gmail.com>
2023-02-05 13:36:55 +0300

    Adding inherited class Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,20 +4,7 @@
 create base class and define rectangle object
 """
 
-
-class BaseGeometry():
-    """ Create BaseGeometry class"""
-
-    def area(self):
-        """raises an Exception with the message area() is not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """method to validate the value"""
-        if type(value) != int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
