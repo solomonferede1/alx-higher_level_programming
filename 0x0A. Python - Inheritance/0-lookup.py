54648d6fdc15e65ca534ceb4800898434c909a96
solomon <solomonf1227@gmail.com>
2023-02-05 06:03:57 +0300

    Finding the attributes of an object

---
+++ 0x0A. Python - Inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+0-lookup module
+Define a function looking for the attributes and variables of the object
+"""
+
+
+def lookup(obj):
+    """0-lookup.py finding the attributes of an object"""
+    return dir(obj)
