2546d8a7a012614bda8245ee001f907f62201174
solomon <solomonf1227@gmail.com>
2023-02-06 10:44:14 +0300

    Adding a square module that inherits some of the feature from Rectangle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+The rectangle module
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """the Square class inherites from the Rectangle class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """initailize the private attribute"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        '''getter method for size'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        '''setter method for size'''
+        self.width = value
+        self.height = value
+    def __str__(self):
+        '''overwriten the __str__ method that inherint from Rectangle'''
+
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.height)
+    
