af622a983a71c65470c034cfc65bed608fcc23d3
solomon <solomonf1227@gmail.com>
2023-02-06 14:20:48 +0300

    Adding a square module that inherits some of the feature from Rectangle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,8 +18,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file:"""
