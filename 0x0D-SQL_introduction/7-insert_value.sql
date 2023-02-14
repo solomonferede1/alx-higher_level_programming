c4282e45f4055ab4c3ed1dda6aa7c07dd51e53e6
solomon1227 <solomonf1227@gmail.com>
2023-02-14 22:53:56 +0300

    Insert in to a table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- a script that inserts a new row in the table first_table
 -- The database hbtn_0c_0 in your MySQL server.
-INSERT TO first_table (id, name)
+INSERT INTO first_table (id, name)
 VALUES (89, 'Best School');
