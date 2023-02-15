eba5b88177c97b8be1e3f2a77ffed596129ffaf1
solomon1227 <solomonf1227@gmail.com>
2023-02-15 05:38:08 +0300

    create a table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,13 +6,5 @@ name VARCHAR(256),
 score INT);
 
 INSERT INTO second_table
-VALUES (1, 'Jhon', 10);
+VALUES (1, 'Jhon', 10), (2, 'Alex', 3), (3, 'Bob', 14), 4, 'George', 8);
 
-INSERT INTO second_table
-VALUES (2, 'Alex', 3);
-
-INSERT INTO second_table
-VALUES (3, 'Bob', 14);
-
-INSERT INTO second_table
-VALUES (4, 'George', 8);
