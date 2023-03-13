1c10fbf8631cbe8d74fe792cd9b7688fc8e3e774
solomon1227 <solomonf1227@gmail.com>
2023-03-13 21:36:37 +0300

    First command argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
 
-const argsLength = (process.argv).length;
-if (argsLength <= 2) {
+if (!(process.argv[2])) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
