c4c6e835d5782578dfd52bc6ee21890fb6295a4d
solomon1227 <solomonf1227@gmail.com>
2023-03-13 22:13:43 +0300

    Print if the command argument is a number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const process = require('process');
 
-if (!(isNaN(+(process.argv[2])))) {
-  console.log(`My number: ${+(process.argv[2])}`);
+if (!(isNaN(parseInt(process.argv[2])))) {
+  console.log(`My number: ${parseInt(process.argv[2])}`);
+} else {
+  console.log('Not a number');
 }
