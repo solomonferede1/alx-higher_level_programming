04853dadc0e6c96af999ca51a39d332bf9d3d485
solomon1227 <solomonf1227@gmail.com>
2023-03-15 23:05:20 +0300

    Concatenate the content of two file and save on the other file

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//  script that concats 2 files.
+
+const files = require('files');
+const file1 = files.readFileSync(process.argv[2], 'utf8');
+const file2 = files.readFileSync(process.argv[3], 'utf8');
+files.writeFileSync(process.argv[4], file1 + file2);
