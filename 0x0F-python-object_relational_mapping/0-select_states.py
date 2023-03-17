acf58df27b75982869ba8d47dfff8d21d1f501aa
solomon1227 <solomonf1227@gmail.com>
2023-03-17 14:43:42 +0300

    read a database table from python script

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,4 +9,5 @@ The 0-select states module
 import MySQLdb
 
 
-db =MYSQLdb.connect(host="localhost", port=3306, user="solomon", passwd="solomon", db="hbtn_0e_0_usa")
+db = MySQLdb.connect(host="localhost", port=3306, user="solomon",
+                          passwd="solomon", db="hbtn_0e_0_usa")
