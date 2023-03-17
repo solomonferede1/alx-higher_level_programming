95f12dd1078b979d8c2f0a72ba0b4b2b3cebd0aa
solomon1227 <solomonf1227@gmail.com>
2023-03-17 14:35:01 +0300

    a script that lists all states from the database hbtn_0e_0_usa:

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+The 0-select states module
+
+"""
+
+
+import MySQLdb
+
+
+db =MYSQLdb.connect(host="localhost", port=3306, user="solomon", passwd="solomon", db="hbtn_0e_0_usa")
