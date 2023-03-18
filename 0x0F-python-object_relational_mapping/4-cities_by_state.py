4f51059dcaabd2b5395a2c270d83ab84c2f5b4bb
solomon1227 <solomonf1227@gmail.com>
2023-03-18 10:13:28 +0300

    a script that lists all cities from the database

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,7 +2,8 @@
 """
 The 4-cities_by_state module
 It lists all cities from the database hbtn_0e_4_usa
-script should take 3 arguments: mysql username, mysql password and database name
+script should take 3 arguments: mysql username, mysql password
+and database name
 """
 
 import MySQLdb
@@ -14,9 +15,9 @@ def _Cities():
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id cities.name states.name FROM cities INNER\
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities INNER\
      JOIN states ON cities.state_id = states.id ORDER BY cities.id")
-    rows = curl.fetchall()
+    rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
