83453b1ab6ec87cc6638ce821665f1925085402c
solomon1227 <solomonf1227@gmail.com>
2023-03-23 22:48:35 +0300

    search sate by name

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+================================
+
+10-model_state_my_get module
+prints the State object with the name passed as argument from the database
+================================
+"""
+
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def search_state_by_name():
+    """prints the first State object from the database"""
+    url = "mysql+mysqldb://{}:{}@localhost:3306/{}"\
+        .format(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(url, echo=False)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State.id).filter(State.name == sys.argv[4])
+    if result is not None:
+        print(result)
+    else:
+        print("Not found")
+
+
+if __name__ == "__main__":
+    search_state_by_name()
