3523dee63a863a684ead2d69191d23223e594a1c
solomonferede <solomonf1227@gmail.com>
2023-03-31 23:29:01 +0300

    Reading url using urllib package

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,12 +5,12 @@ fetches https://alx-intranet.hbtn.io/status
 """
 
 
-from urllib import request
+import urllib
 
 
-with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
-print("Body response:")
-print("\t- type: ", type(html))
-print("\t- content: ", html)
-print("\t- utf8 content: ", html.decode('utf-8'))
+print('Body response:')
+print('    - type: ', type(html))
+print('    - content: ', html)
+print('    - utf8 content: ', html.decode('utf-8'))
