e97eb961e02c4219a10af904458ebc46338fa095
solomonferede <solomonf1227@gmail.com>
2023-04-02 18:51:04 +0300

    fetches url using urllib package

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
     print('Body response:')
-    print('\t- type: ', type(html))
-    print('\t- content: ', html)
-    print('\t- utf8 content: ', html.decode('utf-8'))
+    print('\t- type:', type(html))
+    print('\t- content:', html)
+    print('\t- utf8 content:', html.decode('utf-8'))
