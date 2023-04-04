ba1608ffe62cd7c2323892b80ab8f8b823a97cc2
solomonferede <solomonf1227@gmail.com>
2023-04-04 13:57:03 +0300

    post an email adress

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-# Post an email - urllib headers
+"""
+2-post_email module
+Post an email - urllib headers
+"""
 
 
 from urllib import request
 import sys
 
 
-url = sys.argv[1]
-Email = sys.argv[2]
-headers = {'email': Email}
+if __name__ == "__main__":
+    url = sys.argv[1]
+    Email = sys.argv[2]
+    headers = {'email': Email}
 
-req = request.Request(url, None, headers)
-with request.urlopen(req) as response:
-    html = response.read()
+    req = request.Request(url, None, headers)
+    with request.urlopen(req) as response:
+        html = response.read()
 
-html = html.decode('utf-8')
-print(html)
+    html = html.decode('utf-8')
+    print(html)
