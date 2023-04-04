4da1d8554525086cdce92ae098638cf78f35310b
solomonferede <solomonf1227@gmail.com>
2023-04-04 16:08:07 +0300

    post an email adress using requests package

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+2-post_email module
+Post an email - urllib headers
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    Email = sys.argv[2]
+    value = {'email': Email}
+
+    resp = requests.get(url, parms=value)
+    print(resp.text)
