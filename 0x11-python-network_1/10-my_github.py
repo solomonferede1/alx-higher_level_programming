e1b44fe517f09980d73da086c93e7196ca45ec3e
solomonferede <solomonf1227@gmail.com>
2023-04-04 23:10:54 +0300

    git hub authentication

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+10-my_github module
+Write a Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = 'https://api.github.com/user'
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    r = requests.get(url, auth={username, token})
+    r_dict = r.json()
+    print('{}'.format(r_dict['id']))
