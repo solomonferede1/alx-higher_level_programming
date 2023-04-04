5747c721e06a186f804f3bc738bfdea73b30e3fb
solomonferede <solomonf1227@gmail.com>
2023-04-04 21:30:02 +0300

    print te response error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,10 +12,7 @@ if __name__ == "__main__":
     import sys
 
     url = sys.argv[1]
-    try:
-        resp = requests.get(url)
-        if resp.status_code < 400:
-            print(resp.text)
-        resp.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print("Error code:", err.args[0])
+    resp = requests.get(url)
+    if resp.ok == False:
+        print(resp.status_code)
+
