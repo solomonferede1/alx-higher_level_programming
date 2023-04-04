1b24558a0dbdaceb414cbb01c8b68a9a4f525254
solomonferede <solomonf1227@gmail.com>
2023-04-04 14:05:39 +0300

    post an email adress

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,16 +5,18 @@ Post an email - urllib headers
 """
 
 
-from urllib import request
+from urllib import request, parse
 import sys
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
     Email = sys.argv[2]
-    headers = {'email': Email}
+    value = {'email': Email}
+    data = parse.urlencode(value)
+    data = data.encode('ascii')
 
-    req = request.Request(url, None, headers)
+    req = request.Request(url, data)
     with request.urlopen(req) as response:
         html = response.read()
 
