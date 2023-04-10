1ce5dbea863daa7e0d52e796cb315e1a639442d2
solomonferede <solomonf1227@gmail.com>
2023-04-10 06:41:16 +0300

    perform calculation of two numbers

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+100-my_calculator
+Perform calculation of two numbers
+"""
+
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    if len(sys.argv) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    else:
+        a = sys.argv[1]
+        b = sys.argv[3]
+        operator = sys.argv[2]
+        if operator == '+':
+            print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+        elif operator == '-':
+            print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+        elif operator == '-':
+            print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+        elif operator == '/':
+            print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
