6d584afcc07c578ff23af9312c7a27903e36e28c
solomonferede <solomonf1227@gmail.com>
2023-04-10 06:44:44 +0300

    perform calculation of two numbers

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
         exit(1)
 
     else:
-        a = sys.argv[1]
-        b = sys.argv[3]
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
         operator = sys.argv[2]
         if operator == '+':
             print("{} {} {} = {}".format(a, operator, b, add(a, b)))
