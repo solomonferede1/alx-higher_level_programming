8f37288469ad86ac44b1176651fd8e8f0cbcb335
solomonferede <solomonf1227@gmail.com>
2023-04-12 16:59:25 +0300

    add attribute if possible

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,6 +6,7 @@ Add attribute to an object if possible
 
 
 def add_attribute(obj, att_name, value):
+    """Add attribute to an object if possible"""
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(obj, att_name, value)
