f7071c37a97e77af499e2bb34b3b9da78ecf160f
solomon <solomon@ubuntu.myguest.virtualbox.org>
2023-06-01 09:25:54 +0300

    list number of records with the same record

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,7 @@
 -- Write a script that lists the number of records with the same score
 -- in the table second_table of the database hbtn_0c_0 in your MySQL server.
-use hbtn_0c_0
-SELECT score, COUNT(*) AS number
+use hbtn_0c_0;
+SELECT score, COUNT(score) AS number
 FROM second_table
 GROUP BY score
 ORDER BY score DESC;
