06038c3136a6435843f412dd22870b48d1ea34b8
solomonferede <solomonf1227@gmail.com>
2023-06-21 09:38:28 +0300

    show all privileges of user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2
 -- on your server (in localhost).
-SELECT * 
-FROM information_schema.user_privileges
-WHERE (GRANTEE LIKE 'user_0d_1@localhost' OR  GRANTEE LIKE 'user_0d_2@localhost');
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
