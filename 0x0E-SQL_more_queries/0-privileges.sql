2ca1342ed6e3cbd0a2466dd21c2c08dec1eecaef
solomonferede <solomonf1227@gmail.com>
2023-06-21 09:36:03 +0300

    show all privileges of user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -2,4 +2,4 @@
 -- on your server (in localhost).
 SELECT * 
 FROM information_schema.user_privileges
-WHERE (GRANTEE LIKE 'user_0d_1'@'localhost' OR  GRANTEE LIKE 'user_0d_2'@'localhost');
+WHERE (GRANTEE LIKE 'user_0d_1@localhost' OR  GRANTEE LIKE 'user_0d_2@localhost');
