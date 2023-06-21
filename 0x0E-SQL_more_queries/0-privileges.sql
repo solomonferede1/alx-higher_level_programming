a1714333251dcc5bee9a561bdcb1cf65cbbd6e0f
solomonferede <solomonf1227@gmail.com>
2023-06-21 10:03:01 +0300

    change mode to execute , - show all privileges of user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2
 -- on your server (in localhost).
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT * 
+FROM mysql.user 
+WHERE User = 'user_0d_1' OR User = 'user_0d_2';
