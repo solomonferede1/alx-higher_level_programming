be9efb64d8ff2a96055b6d9a95901092230866de
ubuntu <ubuntu@ubuntu.ethiotech.tech>
2023-07-09 22:46:16 +0300

    Convert to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,8 +5,7 @@ The 8-class_to_json which converts object to json
 import json
 
 
-def load_from_json_file(filename):
+def class_to_json(obj):
     """Convert to json"""
 
-    with open(filename, "r", encoding="utf-8") as fp:
-        return json.load(fp)
+    return vars(obj)
