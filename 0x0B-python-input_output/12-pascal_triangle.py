773ef44e61e289e1ba1ca9557b2af48f3eac28ed
solomonferede <solomonf1227@gmail.com>
2023-07-10 22:56:52 +0300

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Interview question - pascal triangle"""
+
+
+def pascal_triangle(n):
+    """Define a function that returns the pascal triangle"""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
