19586a74745812265eb24f81549217d427ff494d
Solomon <solomonf1227@gmail.com>
2024-07-30 11:50:26 -0700

    Semistandard correction

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,8 +3,7 @@
 const fs = require('fs');
 
 // Get the file path from command-line arguments
-const filePath = process.argv[1];
-
+const filePath = process.argv[2];
 
 // Read the file content
 fs.readFile(filePath, 'utf8', (err, data) => {
