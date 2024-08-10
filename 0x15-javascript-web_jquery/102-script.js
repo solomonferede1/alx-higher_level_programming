5aa0ebe342b11a49cc48baaea4615bb4755e59e4
solomonferede1 <solomonf1227@gmail.com>
2024-08-11 00:36:15 +0300

    Say hello to everyone

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+  $('#btn_translate').click(function() {
+    const langCode = $('#language_code').val();
+    const apiUrl = `https://www.fourtonfish.com/hellosalut/hello/?lang=${langCode}`;
+
+      $.get(apiUrl, function(data) {
+        $('#hello').text(data.hello);
+      });
+  });
+});
